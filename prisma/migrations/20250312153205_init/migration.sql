c9458a54ccd3246d4fb454048d7d9b9e34f85da7
Return_归来 <1140444693@qq.com>
2025-03-12 23:33:13 +0800

    调整数据库迁移模型，支持多文件

--- prisma/migrations/20250312153205_init/migration.sql
+++ prisma/migrations/20250312153205_init/migration.sql
@@ -1,4 +1,92 @@
 -- CreateTable
+CREATE TABLE `cms_article` (
+    `article` INTEGER NOT NULL AUTO_INCREMENT,
+    `post_format` TINYINT NULL DEFAULT 1,
+    `seo_title` VARCHAR(100) NULL,
+    `seo_keywords` VARCHAR(255) NULL,
+    `seo_description` VARCHAR(255) NULL,
+    `thumbnail` VARCHAR(255) NULL,
+    `title` VARCHAR(100) NOT NULL,
+    `content` LONGTEXT NULL,
+    `keywords` VARCHAR(255) NULL,
+    `excerpt` VARCHAR(255) NULL,
+    `author` VARCHAR(50) NULL,
+    `source` VARCHAR(255) NULL,
+    `is_top` TINYINT NULL,
+    `hits` INTEGER NULL,
+    `favorites` INTEGER NULL,
+    `likes` INTEGER NULL,
+    `comments` INTEGER NULL,
+    `more` JSON NULL,
+    `article_status` TINYINT NULL DEFAULT 1,
+    `comment_status` TINYINT NULL DEFAULT 1,
+    `order` INTEGER NULL,
+    `published_at` INTEGER NULL,
+    `create_id` INTEGER NOT NULL,
+    `creator` VARCHAR(50) NOT NULL,
+    `update_id` INTEGER NOT NULL,
+    `updater` VARCHAR(50) NOT NULL,
+    `created_at` INTEGER NOT NULL,
+    `updated_at` INTEGER NOT NULL,
+    `deleted_at` INTEGER NOT NULL DEFAULT 0,
+
+    PRIMARY KEY (`article`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `cms_article_category` (
+    `articleCategoryId` INTEGER NOT NULL AUTO_INCREMENT,
+    `parent_id` INTEGER NOT NULL,
+    `seo_title` VARCHAR(100) NULL,
+    `seo_keywords` VARCHAR(255) NULL,
+    `seo_description` VARCHAR(255) NULL,
+    `name` VARCHAR(100) NOT NULL,
+    `icon` VARCHAR(255) NULL,
+    `description` VARCHAR(255) NULL,
+    `status` TINYINT NULL DEFAULT 1,
+    `article_count` INTEGER NOT NULL,
+    `path` VARCHAR(255) NOT NULL,
+    `order` INTEGER NULL,
+    `create_id` INTEGER NOT NULL,
+    `creator` VARCHAR(50) NOT NULL,
+    `update_id` INTEGER NOT NULL,
+    `updater` VARCHAR(50) NOT NULL,
+    `created_at` INTEGER NOT NULL,
+    `updated_at` INTEGER NOT NULL,
+    `deleted_at` INTEGER NOT NULL DEFAULT 0,
+
+    PRIMARY KEY (`articleCategoryId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `cms_article_category_post` (
+    `article_id` INTEGER NOT NULL,
+    `article_category_id` INTEGER NOT NULL,
+    `order` INTEGER NULL,
+
+    UNIQUE INDEX `cms_article_category_post_article_id_article_category_id_key`(`article_id`, `article_category_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `cms_article_tag` (
+    `tagId` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `status` INTEGER NOT NULL DEFAULT 1,
+    `article_count` INTEGER NOT NULL,
+
+    UNIQUE INDEX `cms_article_tag_name_key`(`name`),
+    PRIMARY KEY (`tagId`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `cms_article_tag_post` (
+    `article_id` INTEGER NOT NULL,
+    `tag_id` INTEGER NOT NULL,
+
+    UNIQUE INDEX `cms_article_tag_post_article_id_tag_id_key`(`article_id`, `tag_id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
 CREATE TABLE `sys_user` (
     `user_id` INTEGER NOT NULL AUTO_INCREMENT,
     `login_name` VARCHAR(191) NULL,
@@ -168,91 +256,34 @@ CREATE TABLE `sys_media_category` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `cms_article` (
-    `article` INTEGER NOT NULL AUTO_INCREMENT,
-    `post_format` TINYINT NULL DEFAULT 1,
-    `seo_title` VARCHAR(100) NULL,
-    `seo_keywords` VARCHAR(255) NULL,
-    `seo_description` VARCHAR(255) NULL,
-    `thumbnail` VARCHAR(255) NULL,
-    `title` VARCHAR(100) NOT NULL,
-    `content` LONGTEXT NULL,
-    `keywords` VARCHAR(255) NULL,
-    `excerpt` VARCHAR(255) NULL,
-    `author` VARCHAR(50) NULL,
-    `source` VARCHAR(255) NULL,
-    `is_top` TINYINT NULL,
-    `hits` INTEGER NULL,
-    `favorites` INTEGER NULL,
-    `likes` INTEGER NULL,
-    `comments` INTEGER NULL,
-    `more` JSON NULL,
-    `article_status` TINYINT NULL DEFAULT 1,
-    `comment_status` TINYINT NULL DEFAULT 1,
-    `order` INTEGER NULL,
-    `published_at` INTEGER NULL,
-    `create_id` INTEGER NOT NULL,
-    `creator` VARCHAR(50) NOT NULL,
-    `update_id` INTEGER NOT NULL,
-    `updater` VARCHAR(50) NOT NULL,
-    `created_at` INTEGER NOT NULL,
-    `updated_at` INTEGER NOT NULL,
+CREATE TABLE `sys_dept` (
+    `dept_id` INTEGER NOT NULL AUTO_INCREMENT,
+    `parent_id` INTEGER NOT NULL DEFAULT 0,
+    `ancestors` VARCHAR(255) NULL,
+    `dept_name` VARCHAR(50) NOT NULL,
+    `sort_order` TINYINT NOT NULL DEFAULT 1,
+    `leader` VARCHAR(20) NULL,
+    `phone` VARCHAR(11) NULL,
+    `email` VARCHAR(50) NULL,
+    `status` TINYINT NOT NULL DEFAULT 1,
+    `created_id` INTEGER NULL,
+    `created_by` VARCHAR(64) NOT NULL DEFAULT '',
+    `created_at` INTEGER NULL,
+    `updated_id` INTEGER NULL,
+    `updated_by` VARCHAR(64) NOT NULL DEFAULT '',
+    `updated_at` INTEGER NULL,
     `deleted_at` INTEGER NOT NULL DEFAULT 0,
 
-    PRIMARY KEY (`article`)
+    INDEX `sys_dept_dept_id_parent_id_dept_name_idx`(`dept_id`, `parent_id`, `dept_name`),
+    PRIMARY KEY (`dept_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
-CREATE TABLE `cms_article_category` (
-    `articleCategoryId` INTEGER NOT NULL AUTO_INCREMENT,
-    `parent_id` INTEGER NOT NULL,
-    `seo_title` VARCHAR(100) NULL,
-    `seo_keywords` VARCHAR(255) NULL,
-    `seo_description` VARCHAR(255) NULL,
-    `name` VARCHAR(100) NOT NULL,
-    `icon` VARCHAR(255) NULL,
-    `description` VARCHAR(255) NULL,
-    `status` TINYINT NULL DEFAULT 1,
-    `article_count` INTEGER NOT NULL,
-    `path` VARCHAR(255) NOT NULL,
-    `order` INTEGER NULL,
-    `create_id` INTEGER NOT NULL,
-    `creator` VARCHAR(50) NOT NULL,
-    `update_id` INTEGER NOT NULL,
-    `updater` VARCHAR(50) NOT NULL,
-    `created_at` INTEGER NOT NULL,
-    `updated_at` INTEGER NOT NULL,
-    `deleted_at` INTEGER NOT NULL DEFAULT 0,
+CREATE TABLE `sys_user_dept` (
+    `user_id` INTEGER NOT NULL,
+    `dept_id` INTEGER NOT NULL,
 
-    PRIMARY KEY (`articleCategoryId`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `cms_article_category_post` (
-    `article_id` INTEGER NOT NULL,
-    `article_category_id` INTEGER NOT NULL,
-    `order` INTEGER NULL,
-
-    UNIQUE INDEX `cms_article_category_post_article_id_article_category_id_key`(`article_id`, `article_category_id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `cms_article_tag` (
-    `tagId` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `status` INTEGER NOT NULL DEFAULT 1,
-    `article_count` INTEGER NOT NULL,
-
-    UNIQUE INDEX `cms_article_tag_name_key`(`name`),
-    PRIMARY KEY (`tagId`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-
--- CreateTable
-CREATE TABLE `cms_article_tag_post` (
-    `article_id` INTEGER NOT NULL,
-    `tag_id` INTEGER NOT NULL,
-
-    UNIQUE INDEX `cms_article_tag_post_article_id_tag_id_key`(`article_id`, `tag_id`)
+    UNIQUE INDEX `sys_user_dept_user_id_dept_id_key`(`user_id`, `dept_id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
