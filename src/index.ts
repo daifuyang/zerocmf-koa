4eadc113e09c041c08dae951d1d8c8c24cc04369
Return_归来 <1140444693@qq.com>
2025-03-20 11:00:59 +0800

    调整swagger的路由地址

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import "dotenv/config";
 import Koa from "koa";
-import { koaSwagger } from "koa2-swagger-ui";
 import bodyParser from "koa-body";
 import koaStatic from "koa-static";
 import router, { registerRoutes } from "@/cmf/route";
@@ -53,15 +52,6 @@ app.use(router.routes());
 
 // 加载swagger.json
 app.use(swaggerRouter.routes());
-// 加载swagger-ui
-app.use(
-  koaSwagger({
-    routePrefix: "/swagger",
-    swaggerOptions: {
-      url: "/swagger.json"
-    }
-  })
-);
 
 app.listen(port, () => {
   console.log(`🚀 Server is running on port http://localhost:${port}/`);
--- src/router/swagger.ts
+++ src/router/swagger.ts
@@ -1,6 +1,8 @@
+import { koaSwagger } from "koa2-swagger-ui";
 import { SWAGGER_APIS } from "@/constants/path";
 import Router from "koa-router";
 import swaggerJSDoc from "swagger-jsdoc";
+import { prefix } from "@/cmf/constants/router";
 
 const router = new Router();
 const options = {
@@ -10,15 +12,29 @@ const options = {
       title: "ZeroCMF API Documentation",
       version: "1.0.0",
       description: "API documentation for ZeroCMF project"
-    },
+    }
   },
   apis: [SWAGGER_APIS] // 这里指定包含Swagger注释的文件路径
 };
 
 const swaggerSpec = swaggerJSDoc(options);
 
-router.get("/swagger.json", (ctx) => {
+const v1 = new Router({ prefix });
+
+v1.get("/swagger.json", (ctx) => {
   ctx.body = swaggerSpec;
 });
+
+v1.get(
+  "/swagger",
+  koaSwagger({
+    routePrefix: false,
+    swaggerOptions: {
+      url: prefix + "/swagger.json"
+    }
+  })
+);
+
+router.use(v1.routes());
 
 export default router;
