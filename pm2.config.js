788652b97b05c6c94bbd8b49d8f40230a4a7fc44
Return_归来 <1140444693@qq.com>
2025-03-20 14:30:51 +0800

    修改pm2部署脚本，支持watch

--- pm2.config.js
+++ pm2.config.js
@@ -5,8 +5,7 @@ module.exports = {
         script: "node index.js", // 使用npm作为启动脚本
         instances: 1,
         autorestart: true,
-        watch: false,
-        max_memory_restart: "1G",
+        watch: true,
         env: {
           NODE_ENV: "production" // 默认设置为生产环境
         }
